2970a453fc6f0c18d18cb7decc8c6c70ed8a57ac
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-28 20:04:28 +0600

    Saving data to firebase

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
+    const [userIngredients, setUserIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
-}
+    const addIngredientHandler = ingredient => {
+        fetch("https://react-hooks-58436.firebaseio.com//ingredients.json", {
+            method: "POST",
+            body: JSON.stringify({ ingredient }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(responseData => {
+            setUserIngredients(prevIngredients => [
+                ...prevIngredients,
+                { id: responseData.name, ...ingredient }
+            ]);
+        });
+    };
+
+    const removeIngredientHandler = id => {
+        setUserIngredients(prevIngredients =>
+            prevIngredients.filter(i => i.id !== id)
+        );
+    };
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler} />
+
+            <section>
+                <Search />
+                <IngredientList
+                    ingredients={userIngredients}
+                    onRemoveItem={id => removeIngredientHandler(id)}
+                />
+            </section>
+        </div>
+    );
+};
 
 export default Ingredients;
